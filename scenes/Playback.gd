c598fc3006adb73f6f90cefc25bd862cfbbfe38e
rjarn <111002047+rjarn@users.noreply.github.com>
2023-04-06 18:22:09 -0400

    test? i dont think this will work how im hoping

--- scenes/Playback.gd
+++ scenes/Playback.gd
@@ -9,3 +9,19 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+	
+	
+
+func _on_button_reload_scene_pressed():
+	
+	# in case the audio glitches and stutters
+	get_tree().reload_current_scene()
+	
+	pass
+
+
+func _on_button_return_main_menu_pressed():
+	
+	get_tree().change_scene_to_file("res://scenes/Home.tscn")
+	
+	pass # Replace with function body.
